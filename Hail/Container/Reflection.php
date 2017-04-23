403e911f8e4bf5f71e1925d584284e90c9aaee36
FlyingHail <flyinghail@msn.com>
2017-04-23 10:56:46 +0800

    better reflection get param type

--- Hail/Container/Reflection.php
+++ Hail/Container/Reflection.php
@@ -3,7 +3,6 @@
 namespace Hail\Container;
 
 use Closure;
-use InvalidArgumentException;
 use ReflectionFunction;
 use ReflectionFunctionAbstract;
 use ReflectionMethod;
@@ -14,58 +13,60 @@ use ReflectionParameter;
  */
 abstract class Reflection
 {
-	/**
-	 * @type string pattern for parsing an argument type from a ReflectionParameter string
-	 *
-	 * @see Reflection::getParameterType()
-	 */
-	const ARG_PATTERN = '/(?:\<required\>|\<optional\>)\\s+([\\w\\\\]+)/';
+    /**
+     * Create a Reflection of the function references by any type of callable (or object implementing `__invoke()`)
+     *
+     * @param callable $callback
+     *
+     * @return ReflectionFunctionAbstract
+     */
+    public static function createFromCallable(callable $callback): ReflectionFunctionAbstract
+    {
+        switch (true) {
+            case is_array($callback):
+                return new ReflectionMethod($callback[0], $callback[1]);
 
-	/**
-	 * Create a Reflection of the function references by any type of callable (or object implementing `__invoke()`)
-	 *
-	 * @param callable $callback
-	 *
-	 * @return ReflectionFunctionAbstract
-	 */
-	public static function createFromCallable(callable $callback)
-	{
-		switch (true) {
-			case is_array($callback):
-				return new ReflectionMethod($callback[0], $callback[1]);
+            case $callback instanceof Closure:
+                return new ReflectionFunction($callback);
 
-			case $callback instanceof Closure:
-				return new ReflectionFunction($callback);
+            case is_object($callback):
+                return new ReflectionMethod($callback, '__invoke');
 
-			case is_object($callback):
-				return new ReflectionMethod($callback, '__invoke');
+            default:
+                return new ReflectionFunction($callback);
+        }
+    }
 
-			default:
-				return new ReflectionFunction($callback);
-		}
-	}
+    /**
+     * Obtain the type-hint of a `ReflectionParameter`, but avoid triggering autoload (as a performance optimization)
+     *
+     * @param ReflectionParameter $param
+     *
+     * @return string|null fully-qualified type-name (or NULL, if no type-hint was available)
+     */
+    public static function getParameterType(ReflectionParameter $param): ?string
+    {
+        if (!$param->hasType()) {
+            return null;
+        }
 
-	/**
-	 * Obtain the type-hint of a `ReflectionParameter`, but avoid triggering autoload (as a performance optimization)
-	 *
-	 * @param ReflectionParameter $param
-	 *
-	 * @return string|null fully-qualified type-name (or NULL, if no type-hint was available)
-	 */
-	public static function getParameterType(ReflectionParameter $param)
-	{
-		if (method_exists($param, 'getType')) {
-			$type = $param->getType();
+        $type = $param->getType();
 
-			return $type === null || $type->isBuiltin()
-				? null // ignore scalar type-hints
-				: $type->__toString();
-		}
+        if ($type === null || $type->isBuiltin()) {
+            return null;
+        }
 
-		if (preg_match(self::ARG_PATTERN, $param->__toString(), $matches) === 1) {
-			return $matches[1];
-		}
+        $type = $type->__toString();
+        $lower = strtolower($type);
 
-		return null; // no type-hint is available
-	}
+        if ($lower === 'self') {
+            return $param->getDeclaringClass()->getName();
+        }
+
+        if ($lower === 'parent' && ($parent = $param->getDeclaringClass()->getParentClass())) {
+            return $parent->getName();
+        }
+
+        return $type;
+    }
 }
